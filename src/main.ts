b8625469d9ca86671614561a98cd805269671334
King Star <mcxin.y@gmail.com>
2024-10-23 19:03:02 +0000

    豁免 请求体过大 的限制 到 3MB

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 请求大小限制豁免 3MB
+  app.use(bodyParser.json({ limit: '3mb' }));
+  app.use(bodyParser.urlencoded({ limit: '3mb', extended: true }));
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
   await app.listen(port);
